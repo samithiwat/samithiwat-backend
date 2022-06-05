c4da42527b6bfbe0fff6b6551fd3387c10348e80
samithiwat <samithiwat2544@gmail.com>
2022-06-05 18:49:16 +0700

    Add RawToDto in helper and common

--- src/service/common.service.go
+++ src/service/common.service.go
@@ -113,3 +113,84 @@ func DtoToRawUser(user *proto.User) *model.User {
 		ImageUrl:    user.ImageUrl,
 	}
 }
+
+func RawToDtoSubTeams(team []*model.Team) []*proto.Team {
+	var subTeams []*proto.Team
+	for _, t := range team {
+		subTeam := proto.Team{
+			Id:          uint32(t.ID),
+			Name:        t.Name,
+			Description: t.Description,
+			SubTeams:    RawToDtoSubTeams(t.SubTeams),
+		}
+		subTeams = append(subTeams, &subTeam)
+	}
+	return subTeams
+}
+
+func RawToDtoTeam(team *model.Team) *proto.Team {
+	subTeams := RawToDtoSubTeams(team.SubTeams)
+
+	return &proto.Team{
+		Id:          uint32(team.ID),
+		Name:        team.Name,
+		Description: team.Description,
+		SubTeams:    subTeams,
+	}
+}
+
+func RawToDtoOrganization(org *model.Organization) *proto.Organization {
+	var roles []*proto.Role
+	for _, role := range org.Roles {
+		roles = append(roles, &proto.Role{Id: uint32(role.ID)})
+	}
+
+	return &proto.Organization{
+		Id:          uint32(org.ID),
+		Name:        org.Name,
+		Description: org.Description,
+		Teams:       RawToDtoSubTeams(org.Teams),
+		Roles:       roles,
+	}
+}
+
+func DtoToRawSubTeams(team []*proto.Team) []*model.Team {
+	var subTeams []*model.Team
+	for _, t := range team {
+		subTeam := model.Team{
+			Model:       gorm.Model{ID: uint(t.Id)},
+			Name:        t.Name,
+			Description: t.Description,
+			SubTeams:    DtoToRawSubTeams(t.SubTeams),
+		}
+		subTeams = append(subTeams, &subTeam)
+	}
+	return subTeams
+}
+
+func DtoToRawTeam(team *proto.Team) *model.Team {
+	return &model.Team{
+		Model:       gorm.Model{ID: uint(team.Id)},
+		Name:        team.Name,
+		Description: team.Description,
+	}
+}
+
+func DtoToRawOrganization(org *proto.Organization) *model.Organization {
+	var roles []*model.Role
+	for _, role := range org.Roles {
+		roles = append(roles, &model.Role{
+			Model:       gorm.Model{ID: uint(role.Id)},
+			Name:        role.Name,
+			Description: role.Description,
+		})
+	}
+
+	return &model.Organization{
+		Model:       gorm.Model{ID: uint(org.Id)},
+		Name:        org.Name,
+		Description: org.Description,
+		Teams:       DtoToRawSubTeams(org.Teams),
+		Roles:       roles,
+	}
+}
--- src/test/helper.go
+++ src/test/helper.go
@@ -57,3 +57,19 @@ func RawToDtoUser(user *model.User) *proto.User {
 		DisplayName: user.DisplayName,
 	}
 }
+
+func RawToDtoTeam(team *model.Team) *proto.Team {
+	return &proto.Team{
+		Id:          uint32(team.ID),
+		Name:        team.Name,
+		Description: team.Description,
+	}
+}
+
+func RawToDtoOrganization(org *model.Organization) *proto.Organization {
+	return &proto.Organization{
+		Id:          uint32(org.ID),
+		Name:        org.Name,
+		Description: org.Description,
+	}
+}
