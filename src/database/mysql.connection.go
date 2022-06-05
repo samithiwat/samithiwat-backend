96518bad62db091126f05bb86bbc8a821a34670c
samithiwat <samithiwat2544@gmail.com>
2022-06-05 13:14:05 +0700

    Create db connection

---
+++ src/database/mysql.connection.go
@@ -0,0 +1,24 @@
+package database
+
+import (
+	"fmt"
+	"github.com/samithiwat/samithiwat-backend/src/config"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDatabase(conf *config.Database) (gormDb *gorm.DB, err error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True", conf.User, conf.Password, conf.Host, conf.Name)
+
+	gormDb, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return
+	}
+
+	//err = gormDb.AutoMigrate(model.User{}, model.Contact{}, model.Location{}, model.Role{}, model.Organization{}, model.Team{})
+	if err != nil {
+		return
+	}
+
+	return
+}
