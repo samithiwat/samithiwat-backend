944b5971261c75efe43f22727736acb9a9406461
samithiwat <samithiwat2544@gmail.com>
2022-06-05 19:12:08 +0700

    Add model in auto migration

--- src/database/mysql.connection.go
+++ src/database/mysql.connection.go
@@ -3,6 +3,7 @@ package database
 import (
 	"fmt"
 	"github.com/samithiwat/samithiwat-backend/src/config"
+	"github.com/samithiwat/samithiwat-backend/src/model"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -15,7 +16,7 @@ func InitDatabase(conf *config.Database) (gormDb *gorm.DB, err error) {
 		return
 	}
 
-	//err = gormDb.AutoMigrate(model.User{}, model.Contact{}, model.Location{}, model.Role{}, model.Organization{}, model.Team{})
+	err = gormDb.AutoMigrate(model.User{}, model.Contact{}, model.Location{}, model.Role{}, model.Permission{}, model.Organization{}, model.Team{})
 	if err != nil {
 		return
 	}
